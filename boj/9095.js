b4fca89c7eda7e792d73e41a54b6e4dfcc89c9cc
June0920 <junpyo0920@gmail.com>
2026-01-30 18:39:16 +0900

    add: [BOJ] 9095
    - DP로 풀이
      - 문제 의도는 다이나믹 프로그래밍
      - 제약 조건과 제한 사항 상 완전 탐색으로 풀이하여도 문제 없음

--- boj/9095.js
+++ boj/9095.js
@@ -3,20 +3,25 @@ const input = fs.readFileSync('/dev/stdin').toString().trim();
 const lines = input.split('\n').map(e => +e);
 const T = lines[0];
 
-for (let i = 1; i <= T; i++) {
-    const answer = dfs(lines[i]);
-    console.log(answer);
+const dp = Array(11).fill(0)
+
+dp[1] = 1;
+dp[2] = 2;
+dp[3] = 4;
+
+function getWays(i) {
+    if (dp[i]) return dp[i];
+
+    dp[i] = getWays(i - 1) + getWays(i - 2) + getWays(i - 3);
+
+    return dp[i];
 }
 
-function dfs(n, acc = 0, arr = []) {
-    if (acc === n) return 1;
-    if (acc > n) return 0;
-
-    let ret = 0;
-
-    for (let i = 1; i <= 3; i++) {
-        ret += dfs(n, acc +i, [...arr, i]);
+for (let i = 1; i <= T; i++) {
+    if (lines[i] < 4) {
+        console.log(dp[lines[i]]);
+        continue;
     }
-
-    return ret;
+    
+    console.log(getWays(lines[i]));
 }
